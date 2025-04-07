34d852e3581b4c3e6c094dac0d894c4c170612a7
siyul-park <syara.park@gmail.com>
2025-04-07 23:23:27 +0900

    refactor: expose plan functions

--- plan/planner.go
+++ plan/planner.go
@@ -15,9 +15,8 @@ func NewPlanner() *Planner {
 
 func (p *Planner) Plan(node sqlparser.Statement) (Plan, error) {
 	switch n := node.(type) {
-	case *sqlparser.Union:
-	case *sqlparser.Select:
-		return p.planSelect(n)
+	case sqlparser.SelectStatement:
+		return p.planSelectStatement(n)
 	case *sqlparser.Insert:
 	case *sqlparser.Update:
 	case *sqlparser.Delete:
@@ -31,14 +30,13 @@ func (p *Planner) Plan(node sqlparser.Statement) (Plan, error) {
 	case *sqlparser.Rollback:
 	case *sqlparser.OtherRead:
 	case *sqlparser.OtherAdmin:
-	case *sqlparser.ParenSelect:
 	case *sqlparser.Stream:
 	}
 	return nil, driver.ErrSkip
 }
 
-func (p *Planner) planSelectStatement(n sqlparser.SelectStatement) (Plan, error) {
-	switch n := n.(type) {
+func (p *Planner) planSelectStatement(node sqlparser.SelectStatement) (Plan, error) {
+	switch n := node.(type) {
 	case *sqlparser.Union:
 	case *sqlparser.Select:
 		return p.planSelect(n)
@@ -48,64 +46,37 @@ func (p *Planner) planSelectStatement(n sqlparser.SelectStatement) (Plan, error)
 
 }
 
-func (p *Planner) planSelect(n *sqlparser.Select) (Plan, error) {
-	input, err := p.planTableExprs(n.From)
-	if err != nil {
+func (p *Planner) planSelect(node *sqlparser.Select) (Plan, error) {
+	if input, err := p.planTableExprs(node.From); err != nil {
 		return nil, err
+	} else if input, err = p.planWhere(input, node.Where); err != nil {
+		return nil, err
+	} else if input, err = p.planGroupBy(input, node.GroupBy); err != nil {
+		return nil, err
+	} else if input, err = p.planHaving(input, node.Having); err != nil {
+		return nil, err
+	} else if input, err = p.planSelectExprs(input, node.SelectExprs); err != nil {
+		return nil, err
+	} else if input, err = p.planOrderBy(input, node.OrderBy); err != nil {
+		return nil, err
+	} else if input, err = p.planLimit(input, node.Limit); err != nil {
+		return nil, err
+	} else {
+		return input, nil
 	}
-
-	if n.Where != nil {
-		input = &FilterPlan{Input: input, Expr: n.Where.Expr}
-	}
-
-	if len(n.GroupBy) > 0 {
-		input = &GroupPlan{
-			Input: input,
-			Exprs: n.GroupBy,
-		}
-	}
-
-	if n.Having != nil {
-		input = &FilterPlan{
-			Input: input,
-			Expr:  n.Having.Expr,
-		}
-	}
-
-	if len(n.SelectExprs) > 0 {
-		input = &ProjectPlan{
-			Input: input,
-			Exprs: n.SelectExprs,
-		}
-	}
-
-	if len(n.OrderBy) > 0 {
-		input = &OrderPlan{
-			Input: input,
-			Exprs: n.OrderBy,
-		}
-	}
-
-	if n.Limit != nil {
-		input = &LimitPlan{
-			Input: input,
-			Exprs: n.Limit,
-		}
-	}
-	return input, nil
 }
 
-func (p *Planner) planTableExprs(n sqlparser.TableExprs) (Plan, error) {
-	if len(n) == 0 {
+func (p *Planner) planTableExprs(node sqlparser.TableExprs) (Plan, error) {
+	if len(node) == 0 {
 		return &NopPlan{}, nil
 	}
 
-	left, err := p.planTableExpr(n[0])
+	left, err := p.planTableExpr(node[0])
 	if err != nil {
 		return nil, err
 	}
 
-	for _, expr := range n[1:] {
+	for _, expr := range node[1:] {
 		right, err := p.planTableExpr(expr)
 		if err != nil {
 			return nil, err
@@ -115,8 +86,8 @@ func (p *Planner) planTableExprs(n sqlparser.TableExprs) (Plan, error) {
 	return left, nil
 }
 
-func (p *Planner) planTableExpr(n sqlparser.TableExpr) (Plan, error) {
-	switch n := n.(type) {
+func (p *Planner) planTableExpr(node sqlparser.TableExpr) (Plan, error) {
+	switch n := node.(type) {
 	case *sqlparser.AliasedTableExpr:
 		return p.planAliasedTableExpr(n)
 	case *sqlparser.ParenTableExpr:
@@ -127,8 +98,8 @@ func (p *Planner) planTableExpr(n sqlparser.TableExpr) (Plan, error) {
 	return nil, driver.ErrSkip
 }
 
-func (p *Planner) planSimpleTableExpr(n sqlparser.SimpleTableExpr) (Plan, error) {
-	switch n := n.(type) {
+func (p *Planner) planSimpleTableExpr(node sqlparser.SimpleTableExpr) (Plan, error) {
+	switch n := node.(type) {
 	case sqlparser.TableName:
 		return p.planTableName(n)
 	case *sqlparser.Subquery:
@@ -156,17 +127,17 @@ func (p *Planner) planAliasedTableExpr(node *sqlparser.AliasedTableExpr) (Plan, 
 	return &AliasPlan{Input: plan, Alias: alias}, nil
 }
 
-func (p *Planner) planParenTableExpr(n *sqlparser.ParenTableExpr) (Plan, error) {
-	return p.planTableExprs(n.Exprs)
+func (p *Planner) planParenTableExpr(node *sqlparser.ParenTableExpr) (Plan, error) {
+	return p.planTableExprs(node.Exprs)
 }
 
-func (p *Planner) planJoinTableExpr(n *sqlparser.JoinTableExpr) (Plan, error) {
-	left, err := p.planTableExpr(n.LeftExpr)
+func (p *Planner) planJoinTableExpr(node *sqlparser.JoinTableExpr) (Plan, error) {
+	left, err := p.planTableExpr(node.LeftExpr)
 	if err != nil {
 		return nil, err
 	}
 
-	right, err := p.planTableExpr(n.RightExpr)
+	right, err := p.planTableExpr(node.RightExpr)
 	if err != nil {
 		return nil, err
 	}
@@ -174,14 +145,14 @@ func (p *Planner) planJoinTableExpr(n *sqlparser.JoinTableExpr) (Plan, error) {
 	plan := &JoinPlan{
 		Left:  left,
 		Right: right,
-		Join:  n.Join,
+		Join:  node.Join,
 	}
 
-	if n.Condition.On != nil {
-		plan.On = n.Condition.On
+	if node.Condition.On != nil {
+		plan.On = node.Condition.On
 	}
-	if n.Condition.Using != nil {
-		plan.Using = n.Condition.Using
+	if node.Condition.Using != nil {
+		plan.Using = node.Condition.Using
 	}
 
 	return plan, nil
@@ -191,6 +162,66 @@ func (p *Planner) planTableName(node sqlparser.TableName) (Plan, error) {
 	return &ScanPlan{Table: node}, nil
 }
 
-func (p *Planner) planSubquery(n *sqlparser.Subquery) (Plan, error) {
-	return p.planSelectStatement(n.Select)
+func (p *Planner) planSubquery(node *sqlparser.Subquery) (Plan, error) {
+	return p.planSelectStatement(node.Select)
+}
+
+func (p *Planner) planWhere(input Plan, node *sqlparser.Where) (Plan, error) {
+	if node != nil {
+		return &FilterPlan{
+			Input: input,
+			Expr:  node.Expr,
+		}, nil
+	}
+	return input, nil
+}
+
+func (p *Planner) planGroupBy(input Plan, node sqlparser.GroupBy) (Plan, error) {
+	if len(node) > 0 {
+		return &GroupPlan{
+			Input: input,
+			Exprs: node,
+		}, nil
+	}
+	return input, nil
+}
+
+func (p *Planner) planHaving(input Plan, node *sqlparser.Where) (Plan, error) {
+	if node != nil {
+		return &FilterPlan{
+			Input: input,
+			Expr:  node.Expr,
+		}, nil
+	}
+	return input, nil
+}
+
+func (p *Planner) planSelectExprs(input Plan, node sqlparser.SelectExprs) (Plan, error) {
+	if len(node) > 0 {
+		return &ProjectPlan{
+			Input: input,
+			Exprs: node,
+		}, nil
+	}
+	return input, nil
+}
+
+func (p *Planner) planOrderBy(input Plan, node sqlparser.OrderBy) (Plan, error) {
+	if len(node) > 0 {
+		return &OrderPlan{
+			Input: input,
+			Exprs: node,
+		}, nil
+	}
+	return input, nil
+}
+
+func (p *Planner) planLimit(input Plan, node *sqlparser.Limit) (Plan, error) {
+	if node != nil {
+		return &LimitPlan{
+			Input: input,
+			Exprs: node,
+		}, nil
+	}
+	return input, nil
 }
